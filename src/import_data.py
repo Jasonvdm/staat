8aebc8ec99c528f5010f972c4421e97a7a4a2871
Jason van der Merwe <jason.merwe@gmail.com>
2014-07-17 19:17:57 -0700

    asdasd

--- src/import_data.py
+++ src/import_data.py
@@ -2,6 +2,6 @@ import csv
 
 
 def get_sentiment_dictionary():
-	sentiment_file = open("../data/inquirerbasic.csv", 'rU')
+	sentiment_file = open("inquirerbasic.csv", 'rU')
 	return csv.DictReader(sentiment_file)
 
