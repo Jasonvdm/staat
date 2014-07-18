91f8372a01a262b0fc7b0ecce23fb81134aaf081
Jason van der Merwe <jason.merwe@gmail.com>
2014-07-17 20:14:19 -0700

    asda

--- src/ImportData.py
+++ src/ImportData.py
@@ -1,6 +1,6 @@
 import csv
 import re
-
+import sys
 
 def get_sentiment_dictionary():
 	sentiment_file = open("../data/inquirerbasic.csv", 'rU')
@@ -12,25 +12,31 @@ def import_tweets():
 	raw_tweets = open("../data/tweets.txt", 'r')
 	for line in raw_tweets:
 		hashtag = line[:line.find(" ")]
-		full_tweets = line[line.find(" "):]
+		full_tweets = line[line.find(" ")+1:]
 		final_tweets = []
 		for tweet in full_tweets.split("||"):
-			final_tweets.append(curate_line(tweet))
-		tweet_dictionary[hashtag] = final_tweets
+			if tweet != '': final_tweets.append(curate_line(tweet))
+		tweet_dictionary[hashtag.lower()] = final_tweets
 	return tweet_dictionary
 	
 
 
 
 def get_tweets(hashtag="#mh17"):
-	print hashtag
 	hashtag = hashtag.lower()
 	tweets = import_tweets()
 	return tweets[hashtag]
 	
 
 def curate_line(tweet):
-	print tweet
-	tweet = re.sub('[^\ \w]', '', tweet)
-	print tweet
-	return tweet
+	return re.sub('[^\ \w]', '', tweet)
+	
+
+
+"""
+commandline interface takes a directory name and gold file.
+It then processes each file within that directory and extracts any
+matching e-mails or phone numbers and compares them to the gold file
+"""
+if __name__ == '__main__':
+    get_tweets()
