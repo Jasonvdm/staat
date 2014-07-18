bcc71abc40f96fd037b7619eac950b2c9a1d0fbf
Jason van der Merwe <jason.merwe@gmail.com>
2014-07-17 19:33:39 -0700

    asdA

--- src/ImportData.py
+++ src/ImportData.py
@@ -5,3 +5,7 @@ def get_sentiment_dictionary():
 	sentiment_file = open("../data/inquirerbasic.csv", 'rU')
 	return csv.DictReader(sentiment_file)
 
+
+def get_tweets(hashtag):
+	return tweets[hashtag]
+	
--- src/import_data.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-
-def get_sentiment_dictionary():
-	sentiment_file = open("inquirerbasic.csv", 'rU')
-	return csv.DictReader(sentiment_file)
-
