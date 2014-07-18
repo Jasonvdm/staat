1fc9592420df3ddb13fdf0d53289e3344b7f856a
Jason van der Merwe <jason.merwe@gmail.com>
2014-07-17 20:02:58 -0700

    getting tweets

--- src/ImportData.py
+++ src/ImportData.py
@@ -1,4 +1,5 @@
 import csv
+import re
 
 
 def get_sentiment_dictionary():
@@ -6,6 +7,30 @@ def get_sentiment_dictionary():
 	return csv.DictReader(sentiment_file)
 
 
-def get_tweets(hashtag):
+def import_tweets():
+	tweet_dictionary = dict()
+	raw_tweets = open("../data/tweets.txt", 'r')
+	for line in raw_tweets:
+		hashtag = line[:line.find(" ")]
+		full_tweets = line[line.find(" "):]
+		final_tweets = []
+		for tweet in full_tweets.split("||"):
+			final_tweets.append(curate_line(tweet))
+		tweet_dictionary[hashtag] = final_tweets
+	return tweet_dictionary
+	
+
+
+
+def get_tweets(hashtag="#mh17"):
+	print hashtag
+	hashtag = hashtag.lower()
+	tweets = import_tweets()
 	return tweets[hashtag]
 	
+
+def curate_line(tweet):
+	print tweet
+	tweet = re.sub('[^\ \w]', '', tweet)
+	print tweet
+	return tweet
