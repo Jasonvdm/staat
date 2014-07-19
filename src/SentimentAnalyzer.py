a40c87743b5bea5105abd56f6f222d940de12c27
bridge-eimon <beimon@stanford.edu>
2014-07-19 10:29:16 -0700

    added check for appended number

--- src/SentimentAnalyzer.py
+++ src/SentimentAnalyzer.py
@@ -9,14 +9,25 @@ def analyze_topic(topic):
 	num_pos = 0;
 	total_num_words = 0;
 	for tweet in tweets:
+		has_positive = False
 		for word in tweet.split():
-			word = word.lower()+"#1"
+			word = word.lower()
 			if word in sentiment_dict:
 				total_num_words+=1	
-				print 
-				if sentiment_dict[word]["Positiv"] != '':
-					print "tes"
-				if sentiment_dict[word]["Positiv"] != '': num_pos+=1
+				if sentiment_dict[word]["Positiv"] != '': 
+					num_pos+=1
+					has_positive = True
+					print word
+			else:
+				for i in range(10):
+					new_word = word +"#" + str(i)
+					if new_word in sentiment_dict:
+						total_num_words+=1	
+						if sentiment_dict[new_word]["Positiv"] != '': 
+							num_pos+=1
+							has_positive = True
+							print new_word
+		if has_positive: print tweet
 	print total_num_words
 	print num_pos
 	print float(num_pos)/float(total_num_words)
