697325cd9bd76547ab6118b9d9ceca3fd7720e3c
Jason van der Merwe <jason.merwe@gmail.com>
2014-07-19 12:48:06 -0700

    Asd

--- src/SentimentAnalyzer.py
+++ src/SentimentAnalyzer.py
@@ -6,6 +6,7 @@ def analyze_topic(topic):
 	sentiment_dict = ImportData.get_sentiment_dictionary()
 	tweets = ImportData.pull_tweets(topic)
 	num_pos = 0;
+	num_neg = 0;
 	total_num_words = 0;
 	for tweet in tweets:
 		has_positive = False
@@ -17,8 +18,10 @@ def analyze_topic(topic):
 					num_pos+=1
 					has_positive = True
 					print word
+				if sentiment_dict[word]["Negativ"] != '':
+					num_neg+=1
 			else:
-				for i in range(10):
+				for i in range(2):
 					new_word = word +"#" + str(i)
 					if new_word in sentiment_dict:
 						total_num_words+=1	
@@ -26,10 +29,12 @@ def analyze_topic(topic):
 							num_pos+=1
 							has_positive = True
 							print new_word
+						if sentiment_dict[new_word]["Negativ"] != '': 
+							num_neg+=1
 		if has_positive: print tweet
-	print total_num_words
+	print num_neg
 	print num_pos
-	print float(num_pos)/float(total_num_words)
+	print float(num_pos)/float(num_neg + num_pos)
 	return 
 
 if __name__ == '__main__':
